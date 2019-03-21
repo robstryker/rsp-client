c8d23db8cf6f0413bec21089b100fcad13756d04
Rob Stryker <rob@oxbeef.net>
2019-03-21 17:01:29 -0400

    Fixing default timeouts again
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/protocol/generated/protocol.ts
+++ src/protocol/generated/protocol.ts
@@ -3,7 +3,7 @@
  */
 export namespace Protocol {
     /* tslint:disable */
-    // Generated using typescript-generator version 2.2.413 on 2019-03-21 13:08:11.
+    // Generated using typescript-generator version 2.2.413 on 2019-03-21 16:58:46.
     
     export interface Attribute {
         type: string;
